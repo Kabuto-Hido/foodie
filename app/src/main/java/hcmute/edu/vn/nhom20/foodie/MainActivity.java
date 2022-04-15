0dbac5dee795bfb9a0d3f6a7fd5bbd4a0bd08ba5
Kabuto-Hido <19110163@student.hcmute.edu.vn>
2022-04-16 00:11:02 +0700

    first commit

---
+++ app/src/main/java/hcmute/edu/vn/nhom20/foodie/MainActivity.java
@@ -0,0 +1,29 @@
+package hcmute.edu.vn.nhom20.foodie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnGetStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGetStart = (Button) findViewById(R.id.btnGetStart);
+
+        btnGetStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
